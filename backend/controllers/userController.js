edf682766ea9a587d2587e5b4c0ab1406ce103d9
alha96 <26628763+alha96@users.noreply.github.com>
2017-10-07 18:02:09 +0200

    backend implementation #3

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,11 +4,9 @@ var User = require('../models/User');
 exports.create_a_user = function (req, res) {
     var user = new User(req.body);
     user.save(function (err, result) {
-        /*console.log (err, JSON.stringify(result));
-        res.send(err);*/
         if (err)
             res.send(err);
         res.json(result);
+        console.log("User created: ", JSON.stringify(result));
     });
-    console.log("User created")
 };

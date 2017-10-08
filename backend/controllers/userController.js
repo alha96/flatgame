00fd1465a16a305bc3d0eb57d15ba412d5a92c67
alha96 <26628763+alha96@users.noreply.github.com>
2017-10-08 10:43:26 +0200

    backend implementation #8

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,19 +3,19 @@ var User = require('../models/User');
 
 
 exports.get_a_user_by_username = function (req, res) {
-    console.log("Test");
     var username = req.query.username;
+    console.log("Username:" +username);
     if(username === null){
         console.log("Please provide a username!");
         res.send({user: fuckit});
     } else {
-        User.findById(username, function (err, user) {
+        User.findById(username, function (err, result) {
             if (err) {
                 res.send(err);
                 console.log("User not found");
             } else {
                 res.json(result);
-                console.log("User found:", JSON.stringify(user))
+                console.log("User found:", JSON.stringify(result))
             }
         })
     }

d6b068ea4a021cfcb27f8d045fa450d2e5703cd6
alha96 <26628763+alha96@users.noreply.github.com>
2017-10-08 12:02:42 +0200

    backend implementation #19

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -50,14 +50,14 @@ exports.create_a_user = function (req, res) {
 
 exports.get_a_user = function (req, res) {
     console.log('Getting a user by id...');
-    var userId = req.params.user_id;
+    var userId = req.params.userId;
     console.log("id:" + userId);
     if (typeof params !== 'undefined' && params !== null) {
         console.log('No userId given!');
         //TODO wie antwortet man richtig?
         res.send('Please provide a userId!');
     } else {
-        User.findById(userId, function (err, result) {
+        User.findOne({_id: userId}, function (err, result) {
             if (err) {
                 res.send(err);
                 console.log('User not found: ' + err);

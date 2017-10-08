20fa031999861c56c63258f1bf9f76841b024841
alha96 <26628763+alha96@users.noreply.github.com>
2017-10-08 11:30:00 +0200

    backend implementation #13

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,35 +2,33 @@ var mongoose = require('mongoose');
 var User = require('../models/User');
 
 function isNull(string) {
-    if (string === null ||
+    return string === null ||
         string === "null" ||
         string.length < 1 ||
         string === undefined ||
-        string === "undefined") {
-        return true;
-    } else {
-        return false;
-    }
+        string === "undefined";
 }
 
 
 exports.get_a_user_by_username = function (req, res) {
+    console.log("Getting a user by username...");
     var username = req.query.username;
     if (isNull(username)) {
         console.log("No username given!");
+        //TODO wie antwortet man richtig?
         res.send('Please provide a username!');
     } else {
         User.findOne({username: username}, function (err, result) {
             if (err) {
-                res.send(err);
                 console.log("User not found: " + err);
+                res.send(err);
             } else {
                 if (result) {
-                    res.json(result);
                     console.log("User found:", JSON.stringify(result))
+                    res.json(result);
                 } else {
-                    res.send(null);
                     console.log("User not found");
+                    res.send(null);
                 }
             }
         })
@@ -51,7 +49,28 @@ exports.create_a_user = function (req, res) {
 };
 
 exports.get_a_user = function (req, res) {
-
+    console.log("Getting a user by id...");
+    var userid = req.params.id;
+    if (isNull(userid)) {
+        console.log("No userid given!");
+        //TODO wie antwortet man richtig?
+        res.send('Please provide a username!');
+    } else {
+        User.findOne({_id: userid}, function (err, result) {
+            if (err) {
+                console.log("User not found: " + err);
+                res.send(err);
+            } else {
+                if (result) {
+                    console.log("User found:", JSON.stringify(result))
+                    res.json(result);
+                } else {
+                    console.log("User not found");
+                    res.send(null);
+                }
+            }
+        })
+    }
 };
 
 exports.update_a_user = function (req, res) {

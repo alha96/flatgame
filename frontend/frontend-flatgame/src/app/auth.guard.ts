51a8b36fe0146c74e928307fa7657770d0c01df8
jonblock <jonas.block@telekom.de>
2017-11-15 14:57:39 +0100

    fix

--- frontend/frontend-flatgame/src/app/auth.guard.ts
+++ frontend/frontend-flatgame/src/app/auth.guard.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Http, Headers, Response} from '@angular/http';
 import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
+import {Location} from "@angular/common";
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -9,14 +10,14 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private http: Http) {
+  constructor(private router: Router, private http: Http, private location: Location) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.http.request('http://arkas.alnilam.uberspace.de/api/auth/session').map(result => {
       return true;
     }).catch(err => {
-      this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+      this.router.navigate(['/login'], {queryParams: {returnUrl: this.location.path()}});
       return [false];
     })
   }

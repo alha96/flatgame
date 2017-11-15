95a7f67396630ebbefb114df5f50fac62fffd710
jonblock <jonas.block@telekom.de>
2017-11-15 10:41:56 +0100

    fix

--- frontend/frontend-flatgame/src/app/auth.guard.ts
+++ frontend/frontend-flatgame/src/app/auth.guard.ts
@@ -9,10 +9,10 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private http: Http) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-//    if (this.http.request('http://arkas.alnilam.uberspace.de/api/auth/session').subscribe((res: Response) => {console.log(res.ok); return res.ok;})) {
-//      // logged in so return true
-//      return true;
-//    }
+    if (this.http.request('http://arkas.alnilam.uberspace.de/api/auth/session').subscribe((res: Response) => {console.log(res.ok); return res.ok;})) {
+      // logged in so return true
+      return true;
+    }
 
     // not logged in so redirect to login page with the return url
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});

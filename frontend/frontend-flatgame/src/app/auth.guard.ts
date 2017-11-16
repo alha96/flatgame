8abefc18a21838f77a2cdacc6025880b187d6803
good-live <larsbueker@gmx.de>
2017-11-16 16:16:53 +0100

    Test

--- frontend/frontend-flatgame/src/app/auth.guard.ts
+++ frontend/frontend-flatgame/src/app/auth.guard.ts
@@ -1,21 +1,27 @@
 import {Injectable} from '@angular/core';
-import {Http, Headers, Response} from '@angular/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {Location} from "@angular/common";
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import {UserService} from "./services/user.service";
+import {User} from "./models/user";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private http: Http, private location: Location) {
+  constructor(private router: Router, private http: HttpClient, private location: Location, private userService: UserService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.http.request('/api/auth/session').map(result => {
-      console.log(result);
+    let observable = this.http.get<User>('/api/auth/session');
+    observable.subscribe(data => {
+      console.log(data);
+      this.userService.currUser = data;
+    });
+    return observable.map(result => {
       return true;
     }).catch(err => {
       this.router.navigate(['/login'], {queryParams: {returnUrl: this.location.path()}});

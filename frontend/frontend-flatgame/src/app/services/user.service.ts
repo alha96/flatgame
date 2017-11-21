84edeef14ddcf5d81aaf92d4c1fb535e4c5a70b0
good-live <good-live@users.noreply.github.com>
2017-11-21 14:04:48 +0100

    Update user.service.ts

--- frontend/frontend-flatgame/src/app/services/user.service.ts
+++ frontend/frontend-flatgame/src/app/services/user.service.ts
@@ -7,7 +7,15 @@ export class UserService {
   constructor() { }
 
   //TODO Remove the default user object
-  private _currUser: User = {profile_image: "https://randomuser.me/api/portraits/men/75.jpg"};
+  private _currUser: User = {
+    _id: "0",
+    profile_image: "https://randomuser.me/api/portraits/men/75.jpg",
+    username: "Demo User",
+    googleid: "0",
+    points: 0,
+    flat: null,
+    email: "test@test.de"
+  };
   set currUser(user: User){
     this._currUser = user;
   };
